2efb4ec4b378cfe3feb19e5f62f5c6a22c573cd5
hamzahassen6 <73426280+hamzahassen6@users.noreply.github.com>
2021-01-19 02:28:32 +0100

    few scripts moving cam and moving ball

---
+++ Assets/scripts/move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{ public Rigidbody2D rb;
+  public  float force=5f;
+    public Vector2 jump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb=gameObject.GetComponent<Rigidbody2D>();
+        jump = new Vector2(0,1) ;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("hmm");
+                rb.AddForce(jump * force,ForceMode2D.Impulse);
+        }
+        
+
+
+      
+    }
+}
